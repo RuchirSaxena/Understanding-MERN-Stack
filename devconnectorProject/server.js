2a46cbb7041643d90a991e733b89c2eb59f5edda
Ruchir Saxena <ruchirsaxena@gmail.com>
2018-10-28 00:05:10 +0530

    Initial Server Side Setup

---
+++ devconnectorProject/server.js
@@ -0,0 +1,10 @@
+//Setting up port
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello1 "));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on ${port}`));
