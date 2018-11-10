b8786209aa1b06beb6324c3d3762798d344064e4
Ruchir <ruchirsaxena@gmail.com>
2018-11-11 01:05:09 +0530

    Adding landing page components

--- devconnectorProject/client/src/App.js
+++ devconnectorProject/client/src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
 
 class App extends Component {
   render() {
-    return <div className="App">hello from react</div>;
+    return (
+      <div className="App">
+        <Navbar />
+        <Landing />
+        <Footer />
+      </div>
+    );
   }
 }
 
