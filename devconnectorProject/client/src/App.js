9709afb91f438e1652cef1ff2f7ae8f1763ef962
Ruchir <ruchirsaxena@gmail.com>
2018-11-19 01:11:40 +0530

    Adding Redux to App

--- devconnectorProject/client/src/App.js
+++ devconnectorProject/client/src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+//React Router
+import { BrowserRouter as Router, Route } from "react-router-dom";
+//Redux imports
+import { Provider } from "react-redux";
+import store from "./store/store";
+
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -10,18 +15,19 @@ import Login from "./components/auth/Login";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
